c56af05c617ebf3eda886d777365cfc324272144
kmw14641 <kmw14641@postech.ac.kr>
2025-11-30 22:10:19 +0900

    feat: implement worker state serialization

---
+++ worker/src/main/scala/global/StateRestoreManager.scala
@@ -0,0 +1,37 @@
+package global
+
+import java.io._
+import global.WorkerState
+import utils.FileManager
+import scala.util.Using
+
+object StateRestoreManager {
+    val stateFileName: String = "worker_state"
+    implicit val outputSubDir: FileManager.OutputSubDir = FileManager.OutputSubDir(FileManager.stateRestoreDirName)
+
+    def isClean(): Boolean = {
+        !new File(FileManager.getFilePathFromOutputDir(stateFileName)).exists()
+    }
+
+    def storeState(): Unit = {
+        FileManager.createDirectoryIfNotExists(FileManager.getFilePathFromOutputDir(""))
+
+        Using(new ObjectOutputStream(new FileOutputStream(FileManager.getFilePathFromOutputDir(stateFileName)))) { oos =>
+            val instance = WorkerState.getInstance
+            oos.writeObject(instance)
+        }.get
+    }
+
+    def restoreState() = {
+        assert(!isClean())
+
+        Using(new ObjectInputStream(new FileInputStream(FileManager.getFilePathFromOutputDir(stateFileName)))) { ois =>
+            val instance = ois.readObject().asInstanceOf[WorkerState]
+            WorkerState.setInstance(instance)
+        }.get
+    }
+
+    def clear(): Unit = {
+        FileManager.delete(FileManager.getFilePathFromOutputDir(stateFileName))
+    }
+}
--- worker/src/main/scala/utils/FileManager.scala
+++ worker/src/main/scala/utils/FileManager.scala
@@ -24,9 +24,10 @@ object FileManager {
   val labelingDirName = "labeled"
   val shuffleDirName = "shuffled"
   val finalDirName = "final"
-  private val inputSubDirNames = Set(memSortDirName, fileMergeDirName, labelingDirName, shuffleDirName, finalDirName)
+  val stateRestoreDirName = "state"
+  private val inputSubDirNames = Set(memSortDirName, fileMergeDirName, labelingDirName, shuffleDirName, finalDirName, stateRestoreDirName)
                                   .map(InputSubDir(_))
-  private val outputSubDirNames = Set(memSortDirName, fileMergeDirName, labelingDirName, shuffleDirName, finalDirName)
+  private val outputSubDirNames = Set(memSortDirName, fileMergeDirName, labelingDirName, shuffleDirName, finalDirName, stateRestoreDirName)
                                   .map(OutputSubDir(_))
 
   private var inputDirs: Seq[String] = Seq.empty
