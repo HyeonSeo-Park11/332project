a1be55dde0927d2d5ac45f246074e3601dee530e
HyeonSeo-Park11 <phs1104@postech.ac.kr>
2025-12-02 13:59:07 +0900

    refactor: move request variable outside of for loop

--- master/src/main/scala/server/SamplingServiceImpl.scala
+++ master/src/main/scala/server/SamplingServiceImpl.scala
@@ -37,19 +37,19 @@ class SamplingServiceImpl(implicit ec: ExecutionContext) {
 
     // Initialize worker channels before assigning ranges
     ConnectionManager.initWorkerChannels(workers.map { case (ip, info) => (ip, info.port) })
-
+    
+    val request = WorkersRangeAssignment(
+      assignments = ranges.map { case ((workerIp, workerPort), (start, end)) =>
+        WorkerRangeAssignment(
+          worker = Some(WorkerNetworkInfo(ip = workerIp, port = workerPort)),
+          range = Some(RangeAssignment(start = start, end = end))
+        )
+      }.toSeq
+    )
+    
     for ((ip, info) <- workers) {
       val assign = Future {
         val stub = WorkerServiceGrpc.stub(ConnectionManager.getWorkerChannel(ip))
-        val request = WorkersRangeAssignment(
-          assignments = ranges.map { case ((workerIp, workerPort), (start, end)) =>
-            WorkerRangeAssignment(
-              worker = Some(WorkerNetworkInfo(ip = workerIp, port = workerPort)),
-              range = Some(RangeAssignment(start = start, end = end))
-            )
-          }.toSeq
-        )
-
         val responseFuture = stub.assignRanges(request)
         try {
           val response = Await.result(responseFuture, 10.seconds)
